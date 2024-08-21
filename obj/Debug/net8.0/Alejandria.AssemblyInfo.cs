58ceaa1c1830fd09c777589b58c91e19c361262a
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-08-21 07:28:02 -0500

    fix: trying to merge

--- obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb219c2d5f8970a4a74e8bc61036f72b3adbb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438bdd7a3b734963d814bddb0f5d19faf369cb39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
