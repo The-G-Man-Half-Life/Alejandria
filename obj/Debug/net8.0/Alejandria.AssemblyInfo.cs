948e80ab27d17cb906ef016014f1fc81f426e8b6
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-08-21 07:44:29 -0500

    feat: creation of the class locations

--- obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
@@ -13,23 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3fe9322ef280850ea6d70414df465555a19bbe")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438bdd7a3b734963d814bddb0f5d19faf369cb39")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58e4fd6a43909d887f5ee12613002a45063cba6")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee481c9e549dd6a06473e21be3cade94045de27c")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58e4fd6a43909d887f5ee12613002a45063cba6")]
->>>>>>> ffb219c2d5f8970a4a74e8bc61036f72b3adbb98
->>>>>>> ee3b561694d962d9bb75649ec97f7f1363c20e26
->>>>>>> 2252ea386cd5491668e0d1bfd7e4843aa20ff40b
->>>>>>> d498d22a2aab74b9d5e9ee41762bf17b5527c206
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c71fddc0ada931a57a9547d4020d30f3203edbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
