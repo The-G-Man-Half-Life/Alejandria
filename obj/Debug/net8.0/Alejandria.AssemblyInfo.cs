441259214f4ab21a5fd32fb8744dce9eab0c89df
MajoPino <majo12pino@gmail.com>
2024-08-21 07:34:22 -0500

    Creation of both the admin and the users layouts

--- obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alejandria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fa542c30759377ac4cc3a8927095d371db0779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c527188c0fb43c3c91508986d3d12bf5eb03c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alejandria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
